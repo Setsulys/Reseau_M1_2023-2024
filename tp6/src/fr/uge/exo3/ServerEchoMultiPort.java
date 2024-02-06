7af0805efeb1c52448698c0936bd057885e7d4f4
LY-IENG Steven <steven.ly412@gmail.com>
2024-02-06 12:58:04 +0100

    tp6

---
+++ tp6/src/fr/uge/exo3/ServerEchoMultiPort.java
@@ -0,0 +1,115 @@
+	package fr.uge.exo3;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.util.logging.Logger;
+
+import javax.xml.crypto.Data;
+
+public class ServerEchoMultiPort {
+    private static final Logger logger = Logger.getLogger(ServerEchoMultiPort.class.getName());
+
+    //private final DatagramChannel dc;
+    private final Selector selector;
+    private final int BUFFER_SIZE = 1024;
+    //private final ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
+    private SocketAddress sender;
+    private final int portInf;
+    private final int portSup;
+    
+    class Context{
+    	
+    	private final ByteBuffer contextBuffer = ByteBuffer.allocate(BUFFER_SIZE);
+    	
+    	public void doRead(SelectionKey key) throws IOException {
+            contextBuffer.clear();
+            var channel = (DatagramChannel) key.channel();
+            var sendFrom =channel.receive(contextBuffer);
+        	if(sendFrom==null) {
+        		logger.warning("The selector decieved us");
+        		return;
+        	}
+        	sender  =sendFrom;
+        	contextBuffer.flip();
+        	key.interestOps(SelectionKey.OP_WRITE);
+    	}
+    	
+        private void doWrite(SelectionKey key) throws IOException {
+        	var channel = (DatagramChannel) key.channel();
+        	channel.send(contextBuffer, sender);
+        	if(contextBuffer.hasRemaining()) {
+        		logger.warning("Packet has not been sended");
+        		return;
+        	}
+        	key.interestOps(SelectionKey.OP_READ);
+        }
+    	
+    }
+
+    public ServerEchoMultiPort(int portInf, int portSup) throws IOException {
+        this.portInf = portInf;
+        this.portSup = portSup;
+        selector = Selector.open();
+        
+        for(var port = portInf; port <= portSup;port++) {
+        	var dc = DatagramChannel.open();
+            dc.bind(new InetSocketAddress(port));
+            dc.configureBlocking(false);
+            dc.register(selector, SelectionKey.OP_READ, new Context());
+        }
+
+    }
+
+    public void serve() throws IOException {
+        logger.info("ServerEcho started on port ");
+        while (!Thread.interrupted()) {
+        	try {
+        		selector.select(this::treatKey);
+        	}catch(UncheckedIOException tunneled) {
+        		throw tunneled.getCause();
+        	}
+        }
+    }
+
+    private void treatKey(SelectionKey key) {
+        try {
+            if (key.isValid() && key.isWritable()) {
+                doWrite(key);
+            }
+            if (key.isValid() && key.isReadable()) {
+                doRead(key);
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
+    }
+
+    private void doRead(SelectionKey key) throws IOException {
+    	var context = (Context) key.attachment();
+    	context.doRead(key);
+    }
+
+    private void doWrite(SelectionKey key) throws IOException {
+    	var context = (Context) key.attachment();
+    	context.doWrite(key);
+    }
+
+    public static void usage() {
+        System.out.println("Usage : ServerEcho port port2");
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            usage();
+            return;
+        }
+        new ServerEchoMultiPort(Integer.parseInt(args[0]),Integer.parseInt(args[1])).serve();
+    }
+}
